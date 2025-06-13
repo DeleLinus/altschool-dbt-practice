5a8300046ce860d6670ab54b28747f74f85d3d15
Ayanwoye Gideon Ayandele <ayangidel@gmail.com>
2025-06-13 03:21:24 +0000

    Done with intermediate models

--- models/staging/stripe/stg_stripes__payment.sql
+++ models/staging/stripe/stg_stripes__payment.sql
@@ -1,7 +1,9 @@
  with payments as (
         select id as payment_id, 
         orderid as order_id, 
-        paymentmethod as payment_method, 
+        paymentmethod as payment_method,
+        status,
+        created as created_at, 
         amount / 100 as amount
         from {{ source("stripe","payment")}}
     )
